75afdceb6bfd87150e6bbfd960d201c0eb6d1c6a
Josue0180 <jooficial14@gmail.com>
2024-08-20 12:32:58 -0300

    banco de dados

--- api.py
+++ api.py
@@ -1,21 +1,34 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///livros.db'
+
+db = SQLAlchemy(app)
+
+class Livro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+
+    with app.app_context():
+        db.create_all()
 
 livros = [
     {'id': 1, 'titulo': '1984', 'autor': 'George Orwell'},
     {'id': 2, 'titulo': 'O Senhor dos Anéis', 'autor': 'J. R. R. Tolkien'},
-    {'id': 3, 'titulo': 'O prisioneiro de Azkhaban', 'autor': 'J. K. Rowling'},
+    {'id': 3, 'titulo': 'O Prisioneiro de Azkhaban', 'autor': 'J. K. Rowling'}
 ]
 
-@app.route('/api/livros', methods= ['GET'])
+@app.route('/api/livros', methods=['GET'])
 def get_livros():
     return jsonify(livros)
 
 @app.route('/api/livros/<int:id>', methods=['GET'])
 def get_livro(id):
     livro = next(
-        (livro for livro in livros if livro['id'] == id),
+        (livro for livro in livros if livro ['id'] == id),
         None
     )
     return jsonify(livro) if livro else ('', 404)
